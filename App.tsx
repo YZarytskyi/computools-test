bc13453b698d581f094332dfd4052a1d3f84d25a
YZarytskyi <yurazar222@gmail.com>
2023-02-12 10:40:16 +0200

    add redux-persist

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { AppRegistry, StatusBar } from "react-native";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import Navigation from "./src/components/Navigation/Navigation";
 import Login from "./src/screens/Login/Login";
 import { useAppSelector } from "./src/store/hooks";
-import { store } from "./src/store/store";
+import { persistor, store } from "./src/store/store";
 
 AppRegistry.registerComponent("computools-test", () => App);
 
 const AppWrapper = () => {
   return (
     <Provider store={store}>
-      <App />
-      <StatusBar backgroundColor = "black"/>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+      <StatusBar backgroundColor="black" />
     </Provider>
   );
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import photosReducer from "./photos/photosSlice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  photos: photosReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    photos: photosReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
